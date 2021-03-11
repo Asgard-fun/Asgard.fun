c24f0397d83113c7ba0e7fdee41413f1f88effe5
Andrew <65760867+VolanDeMor1@users.noreply.github.com>
2021-03-11 23:04:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta name="description" content="The most advanced minecraft server. We make the future the present, and the old the future"
+    <meta name="keywords" content="Майнкрафт сервер, майнкрафт сервер, Майнкрафт, майнкрафт, Minecraft, Minecraft server, minigames, roleplay, role-play, рп, роле-плей, Asgard, asgard, asgard.fun, Asgard.fun, Асгард, асгард, Азгард, азгард"
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Asgard</title>
+</head>
+<body>
+    <img class="full-screen" src="./img/Banner_Asgard_01.png" alt="asgard" draggable="false">
+</body>
+</html>
